4a086f4482988fd8293f854d9e2edd08cfa50a40
codehz <git@hertz.moe>
2023-12-27 14:16:31 +0800

    add lazy

--- client.tsx
+++ client.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { lazy as oldLazy, useEffect, useState } from "react";
 
+/** @deprecated use lazy with suspense */
 export function NoSSR({
   children,
   fallback,
@@ -10,4 +11,16 @@ export function NoSSR({
   const [state, setState] = useState<React.ReactNode>(fallback);
   useEffect(() => setState(children), [children]);
   return <>{state}</>;
+}
+
+export function lazy<T>(
+  importFunc: () => Promise<{ default: React.ComponentType<T> }>
+): React.ComponentType<T> {
+  const LazyComponent = oldLazy(importFunc);
+  return (props: any) => {
+    if (typeof window === "undefined") {
+      throw new Error("client only");
+    }
+    return <LazyComponent {...props} />;
+  };
 }
